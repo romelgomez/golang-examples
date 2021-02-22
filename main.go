c0c02aaf098a2fa11f9cd4be1b16b940e0da22ba
Romel Javier Gomez Herrera <bmxandcode@gmail.com>
2021-02-21 20:02:22 -0500

    feat: add maps examples

--- main.go
+++ main.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-	"github.com/romelgomez/golang-hello-mod/flags"
-	"github.com/romelgomez/golang-hello-mod/basic"
-	"github.com/romelgomez/golang-hello-mod/loops"
-	"github.com/romelgomez/golang-hello-mod/arrays"
-	"fmt"
+  "github.com/romelgomez/golang-hello-mod/flags"
+  "github.com/romelgomez/golang-hello-mod/basic"
+  "github.com/romelgomez/golang-hello-mod/loops"
+  "github.com/romelgomez/golang-hello-mod/arrays"
+  "github.com/romelgomez/golang-hello-mod/maps"
+  "fmt"
 )
 
 var Version = "development"
 
 func main() {
-    fmt.Println("Version:\t", Version)
-    fmt.Println("flags.Time:\t", flags.Time)
-    fmt.Println("flags.User:\t", flags.User)
-    fmt.Println("flags.Date:\t", flags.Date)
+  fmt.Println("Version:\t", Version)
+  fmt.Println("flags.Time:\t", flags.Time)
+  fmt.Println("flags.User:\t", flags.User)
+  fmt.Println("flags.Date:\t", flags.Date)
 
-	fmt.Println("\n..:: Main ::..")
+  fmt.Println("\n..:: Main ::..")
 
-	basic.Basic()
-	loops.Loops()
-	arrays.Arrays()
+  basic.Basic()
+  loops.Loops()
+  arrays.Arrays()
+  maps.Maps()
 }
